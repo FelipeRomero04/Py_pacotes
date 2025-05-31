8b1bdfd8d916b6b57e0fe4392e852fe1ecaf512b
Felipe Romero <felipermrcosta@gmail.com>
2025-05-31 15:59:22 -0300

    a

--- pacote/modulos/moeda.py
+++ pacote/modulos/moeda.py
@@ -2,27 +2,27 @@ def aumentar(preco, porcent, f=True):
     more = preco + (preco * porcent / 100)
 
     if f:
-        return (f'${more}')
+        return (f'${more:.2f}')
     return more
 
 def diminuir(preco, porcent, f=True):
     less = preco - (preco * porcent / 100)
     if f:
-        return (f'${less}')
+        return (f'${less:.2f}')
     return less
 
 def dobro(preco, f=True):
     double = preco * 2
 
     if f:
-        return(f'${double}')
+        return(f'${double:.2f}')
     return double
 
 def metade(preco, f=True):
     half = preco / 2
 
     if f: #formatar com cifrão ou não
-        return (f'${half}')
+        return (f'${half:.2f}')
     return half
 
 
@@ -42,6 +42,7 @@ def resumo(p, porc_more, porc_less, f=True):
     print('-' * 30)
     for k, v in list:
         v = str(v).replace('.', ',')
+      
         print(f'{k:<20} {v}')
     print('-' * 30)
     
